e587d4e73f522e91bf9d5c4e15416910be41b2d9
root <root@localhost.localdomain>
2019-02-20 17:14:36 -0500

    Updated comments

--- wiki-crawler.py
+++ wiki-crawler.py
@@ -5,19 +5,25 @@ import requests, sys, time
 from bs4 import BeautifulSoup as bs
 from bs4 import SoupStrainer
 
+#Print path with character replacements
 def prettyPath(path):
 	for url in path:
 		print(url.replace("_"," ")[6:])
 
 	
 def breadthFirstSearch(startURL,destURL):
-
+	
+	#Set frontier
 	frontier = Frontier(startURL)
 	
+	#While there are unexplored nodes
 	while len(frontier.unexplored) > 0:
+		#get current node to expand
 		curNode = frontier.unexplored.pop(0)
+		#For all neighbors of current node that have not been explored
 		for neighbor in fetchOutgoingLinks(wikiBase+curNode.url):
 			if neighbor not in frontier.explored:
+				
 				#Create new node with url and pointer back to parent
 				newNode = Node(neighbor,curNode)
 				
@@ -27,14 +33,17 @@ def breadthFirstSearch(startURL,destURL):
 				#Otherwise update unexplored and explored
 				frontier.unexplored.append(newNode)
 				frontier.explored.append(neighbor)
-	return []
-		
-#Poopy Trash Garbage. Doesn't work cuz im dumb boy
+	#If frontier runs out, return failed path
+	return ['No Path']
+
+#Bi-directional breadth first search		
 def biDirectional(startURL,destURL):
 
+	#Define frontiers
 	forFront = Frontier(startURL) #Forward search from startURL
 	invFront = Frontier(destURL) #Inverted search from destURL
 
+	#While frontiers have unexplored nodes
 	while(len(forFront.unexplored) > 0 and len(invFront.unexplored) > 0):
 		#Forward search from start url
 		curNode = forFront.unexplored.pop(0)
@@ -63,13 +72,16 @@ def biDirectional(startURL,destURL):
 			#Otherwise update unexplored and explored
 			invFront.unexplored.append(newNode)
 			invFront.explored.append(neighbor)
-	return []		
-					
+	return ['No Path']		
+
+#assert that a correct number of arguments are given					
 assert(len(sys.argv) >= 3)
 
+#Set starting and ending urls for search
 startURL = '/wiki/' + sys.argv[1].replace(" ","_")
 destURL = '/wiki/' + sys.argv[2].replace(" ","_")
 
+#Check if verbose flag is set
 if("-v" in sys.argv or "--verbose" in sys.argv):
 	verbose = True
 	#Clean this up later
@@ -84,13 +96,20 @@ if("-v" in sys.argv or "--verbose" in sys.argv):
 else:
 	verbose = False
 
+#Print information if verbose
 if verbose:
 	print("Start URL:",startURL)
 	print("Destination URL:",destURL)
 	print("Beginning Search...")
 	startTime = time.time()
+
+#Run search
 path = biDirectional(startURL,destURL)
+
+#Print search time and data
 if verbose: print("Best path found in {} seconds!\n".format(time.time()-startTime))
+
+#Print path
 prettyPath(path)
 
 
